3b348f5e063c94f252ea26a2180729fab4703447
Ubuntu <ubuntu@ip-172-31-5-3.sa-east-1.compute.internal>
2019-11-02 22:18:30 -0300

    sexta edição

--- producer/producer.py
+++ producer/producer.py
@@ -7,7 +7,7 @@ import json
 
 print('Conectando ao mqtt broker...')
 mqtt_client = mqtt.Client()
-mqtt_client.connect('18.228.116.164', 1883, 60)
+mqtt_client.connect('18.228.241.247', 1883, 60)
 
 while True:
     temperatura = random.uniform(17,30)
